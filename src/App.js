c56e9126b1dfb341783600872367af340f76148f
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-05-25 11:30:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import FeedbackData from "./data/FeedbackData";
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<FeedbackItem />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div style={{ textDecoration: "none", color: "#FF4893" }}>
+          <h2>Feedback Rating UI</h2>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
