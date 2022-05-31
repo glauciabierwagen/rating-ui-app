5efb7055bfffe9ea3ee1489c4c5a008cc6be6a90
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-05-31 14:57:26 +0200

    Adding functions

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import FeedbackData from "./data/FeedbackData";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   console.log(feedback);
+
+  const deleteFeedback = (id) =>{
+    if(window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+      //we will return an array  minus the one we are deleting
+    }
+  }
 
   return (
     <BrowserRouter>
       <Header />
       <div className="container">
         <Routes>
-          <Route exact path="/" element={<FeedbackItem />} />
+          <Route 
+          exact 
+          path="/" 
+          element={<FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>}
+          />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
+import {FaTimes} from "react-icons/fa"
+import Card from "./Card";
 
-const FeedbackItem = () => {
-  const [rating, setRating] = useState(10);
-  const [text, setText] = useState("This is an example of a feedback item");
+const FeedbackItem = ({ item, handleDelete }) => {
+  //const [rating, setRating] = useState(8);
+  //const [text, setText] = useState("This is an example of a feedback item");
   return (
-    <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-    </div>
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close"> <FaTimes color="#000089"/></button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+   
   );
 };
 
